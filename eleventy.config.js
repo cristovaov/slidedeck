de3e47b534e3f92635ff4b2c88a1c0420af0c67c
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-07-25 11:02:16 +0200

    Identity and slides collection

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,35 +3,27 @@ import baseline, { config as baselineConfig } from '@apleasantview/eleventy-plug
 const siteUrl = process.env.URL || 'http://localhost:8080/';
 
 const settings = {
-	title: 'My Site',
+	title: 'eleventy-baseline x reveal.js',
 	url: siteUrl,
 	defaultLanguage: 'en',
 	head: {
-		link: [{
-			rel: 'stylesheet',
-			href:  '/assets/css/index.css'
-		}],
-		script: [
-			{
-				src: '/assets/js/index.js',
-				defer: true
-			}
-		]
+		link: [{ rel: 'stylesheet', href: '/assets/css/index.css' }],
+		script: [{ src: '/assets/js/index.js', defer: true }]
 	}
 };
 
 export default async function (eleventyConfig) {
-	await eleventyConfig.addPlugin(
-		baseline(settings, {
-			head: {
-				showGenerator: true
-			}
-		})
-	);
+	await eleventyConfig.addPlugin(baseline(settings, { head: { showGenerator: true } }));
 
 	eleventyConfig.addGlobalData('settings', settings);
 
 	// Site-specific passthroughs, filters, shortcodes, and collections go here.
+	eleventyConfig.addCollection('slides', function (collectionApi) {
+		const slides = collectionApi.getFilteredByGlob('src/slides/**/*.md');
+		return slides.sort(function (a, b) {
+			return a.inputPath.localeCompare(b.inputPath);
+		});
+	});
 }
 
 export const config = baselineConfig;
